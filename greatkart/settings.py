71e04f92bd8c5fba9b84fa61b6cdb17c4bc9517a
alaya-gill <achristopher@ssidecisions.com>
2022-09-26 22:58:48 +0500

    Account Module Completed

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -41,7 +42,7 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
-    'carts'
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,16 @@ STATICFILES_DIRS = ['greatkart/static',]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+MESSAGE_TAGS = {
+    messages.INFO: 'danger',
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL='gillalaya@gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'gillalaya@gmail.com'
+EMAIL_HOST_PASSWORD = 'rpjxyptehcfspsgb'
